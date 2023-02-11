0fd75484f4a887711f7df68172c20d997aa65088
Matheus Hernandes <12698656+onhernandes@users.noreply.github.com>
2023-02-11 03:08:48 -0300

    Add basic-auth middleware

--- src/middlewares/useAppVersionHeader/index.ts
+++ src/middlewares/useAppVersionHeader/index.ts
@@ -3,13 +3,16 @@
 import * as Koa from "koa";
 import type { SuperKoaFn, SuperKoaOptions } from "../../types";
 
-const useAppVersionHeader: SuperKoaFn = (app: Koa, options: SuperKoaOptions) =>
-  app.use((ctx, next) => {
+export const appVersionHeader =
+  (options: SuperKoaOptions) => (ctx: Koa.Context, next: Koa.Next) => {
     if (options.appVersion && options.appVersionHeader && options.appVersion) {
       ctx.set(options.appVersionHeader, options.appVersion);
     }
 
     return next();
-  });
+  };
+
+const useAppVersionHeader: SuperKoaFn = (app: Koa, options: SuperKoaOptions) =>
+  app.use(appVersionHeader(options));
 
 export default useAppVersionHeader;
